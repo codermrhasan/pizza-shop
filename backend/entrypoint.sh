412f735a4c4ae96624349bf7454d139b24ae6304
Md Rakibul Hasan <rahacode@gmail.com>
2020-01-23 19:26:43 +0600

    setting up docker container with backend

---
+++ backend/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+cd /usr/src/app/backend
+
+python manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
+
+# when create a new script file make sure your script file is executable before copy all files with,
+# chmod +x your/executable/file.sh
